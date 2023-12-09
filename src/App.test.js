fe8d0ead1bf5608d3dcc98ad0cab4044bc273d6b
Nakakandebr <bridgetnakakandea@gmail.com>
2023-12-09 02:06:53 -0800

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PhoneList from './PhoneList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([{ id: 1, country: 'Cameroon', country_code: '+237', number: '123456789' }])
+  })
+);
+
+describe('PhoneList Component', () => {
+  it('renders phone numbers correctly', async () => {
+    render(<PhoneList />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Cameroon - +237 123456789')).toBeInTheDocument();
+    });
+  });
+
+  it('handles fetch error', async () => {
+    global.fetch.mockImplementationOnce(() => Promise.reject('Fetch error'));
+
+    render(<PhoneList />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Error fetching phone numbers: Fetch error')).toBeInTheDocument();
+    });
+  });
 });
